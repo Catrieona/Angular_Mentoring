20326ae445560d6623cc73addc0f21612189653c
ESinitsyn <esinitsyn@cpaglobal.com>
2019-11-20 19:52:57 +0300

    fix: correct code style

--- videoCourse/src/app/components/course-page/course-page-list/course-list-item/item-border-style.directive.ts
+++ videoCourse/src/app/components/course-page/course-page-list/course-list-item/item-border-style.directive.ts
@@ -6,36 +6,37 @@ import { Directive, ElementRef, Renderer2, Input, OnInit } from '@angular/core';
 export class ItemBorderStyleDirective implements OnInit {
   @Input() creationDate;
   public currentDate =  new Date('2019-11-12');
-  
+  public twoWeekTerm = 14 * 24 * 60 * 60 *  1000;
+  public creationDay: number;
+  public currentDay: number;
+
   constructor(
-    private renderer: Renderer2,
-    private el: ElementRef
-    ) {
-  }
+    private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit() {
 
-this.currentDate.getDate();
+  this.currentDate.getDate();
+  this.creationDay = this.creationDate.valueOf();
+  this.currentDay = this.currentDate.valueOf();
 
-if (
-        (this.creationDate.valueOf() < this.currentDate.valueOf()) && 
-        (this.creationDate.valueOf() >= (this.currentDate.valueOf() - 1209600000))
-      ) {
+  if (
+      (this.creationDay < this.currentDay) &&
+      (this.creationDay >= (this.currentDay - this.twoWeekTerm))
+    ) {
         this.renderer.setStyle(
-          this.el.nativeElement,
-          'border',
-          '3px solid #0c8a1f'
+        this.el.nativeElement,
+        'border',
+        '3px solid #0c8a1f'
         );
-
       } else if (
-        this.creationDate.valueOf() > this.currentDate.valueOf()
-      ) {
-        this.renderer.setStyle(
+          this.creationDay > this.currentDay
+        ) {
+          this.renderer.setStyle(
           this.el.nativeElement,
           'border',
           '3px solid #006bb8'
         );
       }
-    }
+  }
 }
 
