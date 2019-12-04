af511fe2a12c9b1d11c5572e6c10d709adcd6347
Kate Makarenka <polumna-la@mail.ru>
2019-12-04 23:47:10 +0300

    fix: fix bug

--- videoCourse/src/app/app.module.ts
+++ videoCourse/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CoursePageSearchComponent } from './components/course-page/course-page-
 import { CoursePageListComponent } from './components/course-page/course-page-list/course-page-list.component';
 import { CourseListItemComponent } from './components/course-page/course-page-list/course-list-item/course-list-item.component';
 import { CoursePageFooterComponent } from './components/course-page/course-page-footer/course-page-footer.component';
+<<<<<<< Updated upstream
 import { ItemBorderStyleDirective } from './core/directives/item-border-style.directive';
 import { CourseDurationPipe } from './shared/pipes/course-duration.pipe';
 import { ConfirmationDelItemComponent } from './shared/confirmation-del-item/confirmation-del-item.component';
@@ -17,6 +18,14 @@ import { LoginModule } from './shared/course-log-in/login.module';
 import { AddNewCourseComponent } from './shared/course-add-new/add-new-course.component';
 import { EditCourseComponent } from './shared/course-edit/edit-course.component';
 import { NotFoundComponent } from './shared/not-found-component/not-found.component';
+=======
+import { ItemBorderStyleDirective } from './components/course-page/course-page-list/course-list-item/item-border-style.directive';
+import { CourseDurationPipe } from './components/course-page/course-page-list/course-list-item/course-duration.pipe';
+import { ConfirmationDelItemComponent } from './components/course-page/course-page-list/confirmation-del-item/confirmation-del-item.component';
+import { LoginModule } from './components/course-log-in/login/login.module';
+import { AddNewCourseComponent } from './components/course-add-new/add-new-course.component';
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -32,8 +41,11 @@ import { NotFoundComponent } from './shared/not-found-component/not-found.compon
     CourseDurationPipe,
     ConfirmationDelItemComponent,
     AddNewCourseComponent,
+<<<<<<< Updated upstream
     EditCourseComponent,
     NotFoundComponent
+=======
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
